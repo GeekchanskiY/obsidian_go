7a6a97a7b653ab12d0cbd29260d37616fcf95ba9
GeekchanskiY <astrovskiy.04@gmail.com>
2024-06-06 04:24:07 +0400

    router init

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -9,33 +9,46 @@ import (
 	"runtime/trace"
 
 	"obsidian_go/internal/database"
+	rt "obsidian_go/internal/router"
 )
 
 func main() {
+
+	// Logging file
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
 	}
+	defer file.Close()
 
+	// Logging to file and stdout
+	logger_output := io.MultiWriter(file, os.Stdout)
+	log.SetOutput(logger_output)
+
+	// Trace file
 	f, err := os.Create("trace.out")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+
+	// Start trace
 	if err := trace.Start(f); err != nil {
 		fmt.Printf("failed to start trace: %v\n", err)
 		return
 	}
 	defer trace.Stop()
-	logger_output := io.MultiWriter(file, os.Stdout)
-	log.SetOutput(logger_output)
 
+	// Connect to database
 	database.Connect()
 
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "About Page")
-	})
+	// Start server
+	r := &rt.Router{}
 
-	fmt.Println("Server is listening...")
-	http.ListenAndServe("localhost:8181", nil)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	server.ListenAndServe()
 }
--- app/internal/router/router.go
+++ app/internal/router/router.go
@@ -1,44 +1,11 @@
 package router
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type Route struct {
-	Handler     http.Handler
-	Methods     []string
-	Middlewares []http.Handler
-}
+type Router struct{}
 
-type SimpleJsonResponse struct {
-	Message string `json:"message"`
-}
-
-func NewRoute(handler http.Handler, methods ...string) *Route {
-	return &Route{
-		Handler:     handler,
-		Methods:     methods,
-		Middlewares: []http.Handler{},
-	}
-}
-
-func MethodNotFoundHandler(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	json.NewEncoder(w).Encode(SimpleJsonResponse{
-		Message: "Method not found"})
-
-}
-
-func (r *Route) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	for _, method := range r.Methods {
-		if req.Method == method {
-			r.Handler.ServeHTTP(w, req)
-			return
-		}
-	}
-
-	MethodNotFoundHandler(w, req)
+func (sr *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
 }
